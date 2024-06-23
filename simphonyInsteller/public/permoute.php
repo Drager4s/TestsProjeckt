873f4a7952c5765b762ee0108eb29b7126ce94d8
roman <egoshin.roma@bk.ru>
2024-06-23 22:38:14 +0300

    добавил счетчик

--- simphonyInsteller/public/permoute.php
+++ simphonyInsteller/public/permoute.php
@@ -1,18 +1,22 @@
 <?php
-function generate_permutations($items, $perms = []) {
+function generate_permutations($items, $perms = [], &$counter = 0) {
     if (empty($items)) {
-        echo join('', $perms) . "\n";
-    }  else {
+        $counter++;
+        return [join('', $perms)];
+    } else {
+        $result = [];
         for ($i = count($items) - 1; $i >= 0; --$i) {
             $newitems = $items;
             $newperms = $perms;
             list($foo) = array_splice($newitems, $i, 1);
             array_unshift($newperms, $foo);
-            generate_permutations($newitems, $newperms);
+            $result = array_merge($result, generate_permutations($newitems, $newperms, $counter));
         }
+        return $result;
     }
 }
 
-$string = "abcd";
-$items = str_split($string);
-generate_permutations($items);
+$counter = 0;
+$perms = generate_permutations(['1', '2', '3'], [], $counter);
+print_r($perms);
+echo "Total operateons: $counter";
